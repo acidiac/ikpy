af8ab92de81ac75d0a26eacf5406dd7bd5aeec56
amit chanchal <amit.chanchal@hotmail.com>
2016-06-27 11:58:59 +0530

    initial cofig

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
 #config
 #This file contains most of the configuration variables that your app needs.
+
+DEBUG = True # Turns on debugging features in Flask
+BCRYPT_LEVEL = 12 # Configuration for the Flask-Bcrypt extension
+MAIL_FROM_EMAIL = "amit.chanchal@sustainurban.org" # For use in application emails
+SECRET_KEY = "tatstslsjkhjhsklckjks65bngfy5"
+MONGO_URI = ""
+MONGO_DBNAME = ""
+
--- ik/__init__.py
+++ ik/__init__.py
@@ -1,1 +1,18 @@
 # This file initializes your application and brings together all of the various components.
+# /ik/__init__.py 
+# author: Amit Chanchal @amitchnchl
+# description: main file for the IK application main module
+# version : 0.0.2
+# Project timeline: June - July 2016
+
+# all the main imports
+from flask import Flask 
+
+app = Flask(__name__)
+# Using the cofig variables define in /config.py
+app.config.from_object('config')
+
+
+
+
+
