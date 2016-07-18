24a20374db58aace9b7a602f8941ae96a615c8fa
amit chanchal <amit.chanchal@hotmail.com>
2016-07-18 16:25:47 +0530

    MG edit test

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # standalone test setup for apis
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
@@ -23,6 +23,32 @@ tasks = [
 def get_tasks():
     return jsonify({'tasks': tasks})
 
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Resource not found'}), 404)
 
 if __name__ == "__main__":
 	app.run(debug=True, port= 5000)
